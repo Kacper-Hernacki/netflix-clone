594cd0c380f4a492b4cfc942c61c596f97bbbc3f
Kacper <hernackikacper@gmail.com>
2020-07-20 22:45:32 +0200

    first commit

---
+++ netflix-clone/build/precache-manifest.6725b22ce67462aa53635b846f25ada4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf37e44aec0e4bea3b3457f30b0d3685",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcf5556f62b42b969fbb",
+    "url": "/static/css/main.065e5375.chunk.css"
+  },
+  {
+    "revision": "53145863f315ed680ce6",
+    "url": "/static/js/2.af068be6.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.af068be6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcf5556f62b42b969fbb",
+    "url": "/static/js/main.d3b5728c.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
